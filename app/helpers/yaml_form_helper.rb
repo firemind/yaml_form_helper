4fdf3d2929bd33bebeacbf185c4b6e98352f051e
Mike <mike@otep.ch>
2011-09-26 21:43:57 +0200

    fix for rails 3.1

--- app/helpers/yaml_form_helper.rb
+++ app/helpers/yaml_form_helper.rb
@@ -2,6 +2,6 @@ module YamlFormHelper
   def yaml_form_for(record, type = 'full', form_options = {}, &proc)
     form_options[:html] = {} if ! form_options[:html]
     form_options[:html].merge!(:class => "yform #{type}")
-    form_for(record, {}, form_options, &proc)
+    form_for(record,  form_options, &proc)
   end
 end
